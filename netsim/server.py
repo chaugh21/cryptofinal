6525e2288c0de304d1e010a052d0048e8c9c77f7
strout18 <stellatrout@mac.com>
2020-04-30 22:53:21 -0500

    corrected stuff in server methods

--- netsim/server.py
+++ netsim/server.py
@@ -6,7 +6,7 @@ from Crypto.Hash import HMAC, SHA256
 '''
 Putting this here for future use. Essentially this is the server side logic for
 generating the derived keys from the nonces sent by the client. This should be
-in the pipeline for the whole message send / reciept / decrypt flow. These keys
+in the pipeline for the whole message send / receive / decrypt flow. These keys
 will be used to decrypt the in app commands sent from the client. This is assuming
 that msg in this case is of type bytes where it has the following format:
 
@@ -30,50 +30,62 @@ if (label == b'mac'):
 class Server:
     def __init__(self):
         self.current_client = "B"
-        self.current_client_dir = "./NETWORK/" + current_client + "/DATA"
-        self.pwds = {}
+        self.current_client_dir = "./NETWORK/A/DATA/" + self.current_client + "/"
 
-    def upload_file(self, filepath):
-        #where are the server files stored??
-        shutil.copy(filepath, "./NETWORK/S/" + self.current_client_dir, True)
+    def upload_file(self, file):
+        if not os.path.exists(self.current_client_dir + file):
+        
 
     def download_file(self, filename):
-        if not os.path.exists("./NETWORK/S" + self.current_client_dir):
-            return ("This file does not exist")
-        shutil.copy("./NETWORKS/S" + self.current_client_dir + "/" + filename, self.current_client_dir, True) #is it right to be copying to network/client/in?
+        path = self.current_client_dir + filename
+        if not os.path.exists(path):
+            print ("This file does not exist")
+        else:
+            f = open(path, "r")
+            msg_str = f.read()
+            msg_bytes = msg_str.encode('ascii')
+
+        
 
     def parse_command(self, plaincomm):
         args = plaincomm.split()
         cmd = (args[0]).upper()
-        if cmd == "MKD":
-            new_dir = self.client_dir + args[1]
+        if cmd == "MKD":    #make directory
+            new_dir = self.current_client_dir + args[1]
             os.mkdir(new_dir)
-            return  #do we want to give result?
-        elif cmd == "RMD":
+        elif cmd == "RMD":  #remove directory
             dir_arg = args[1]
-            if not os.path.exists(self.client_dir + dir_arg):
+            if not os.path.exists(self.current_client_dir + dir_arg):
                 print ("This folder does not exist!")
             else:
-                shutil.rmtree(self.client_dir + dir_arg, ignore_errors=True)
-            return
-        elif cmd == "GWD":
-            return os.getcwd() #check
-        elif cmd == "CWD":
-            os.chdir(self.client_dir + args[1])  #check if this is allowed and if it exists
-            return
-        elif cmd == "LST":
-            return os.listdir() #type list
-        elif cmd == "UPL":
-            self.upload_file(args[1])
-            return
-        elif cmd == "DNL":
-            self.download_file(args[1], dst)
-            return
-        elif cmd == "RMF":
-            os.remove(PATH + args[2] + "/" + args[1])   #check formatting
-            return
+                shutil.rmtree(self.current_client_dir + dir_arg, ignore_errors=True)
+        elif cmd == "GWD":  #get working directory
+            msg_bytes = self.current_client_dir.encode('ascii')
+            #send message! TODO
+        elif cmd == "CWD":  #change directory
+            dir_arg = args[1]
+            path = self.current_client_dir + dir_arg
+            print (path)
+            if not os.path.exists(path) or (not (self.current_client + "..") in path):
+                #tries to make sure user can't back out of their own folder into other folders - kinda hacky
+                print ("This folder does not exist!")
+            else:
+                self.current_client_dir = self.current_client_dir + dir_arg
+        elif cmd == "LST": #list contents
+            lst = os.listdir(self.current_client_dir)
+            msgstr = "\t".join(lst)
+            msg_bytes = msgstr.encode('ascii')
+            #SEND MESSAGE TODO
+        elif cmd == "UPL":  
+            self.upload_file(args[1])   #TODO?? i am confused on how to do this
+        elif cmd == "DNL":  #download file
+            self.download_file(args[1])
+        elif cmd == "RMF":  #rm file from folder
+            print (self.current_client_dir + args[2] + + "/" + args[1])
+            os.remove(self.current_client_dir + args[2] + + "/" + args[1])   #check formatting
         else:
-            return "Command not found"
+            msg_str = "Command not found"
+            msg_bytes = msg_str.encode('ascii')
 
     '''
     Uses the nonce sent over from the client to generate the derived message or
